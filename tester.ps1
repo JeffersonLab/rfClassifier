eabbe763a8568bc90806e23515e71a9f8d1f391a
Adam Carpenter <adamc@jlab.org>
2019-07-31 11:36:51 -0400

    Updated PYTHONPATH and location of models

--- tester.ps1
+++ tester.ps1
@@ -12,8 +12,8 @@ echo "# Running model tests"
 echo "#"
 
 # Add the model directory to the environment's PYTHONPATH.  This allows for all of the needed imports.
-$env:PYTHONPATH = "C:\Users\adamc\code\config\rf_classifier\v1\models"
-python -m unittest discover \Users\adamc\code\config\rf_classifier\v1 -v
+$env:PYTHONPATH = "C:\Users\adamc\code\rf_classifier\models;C:\Users\adamc\code\rf_classifier\venv\Lib\site-packages"
+python -m unittest discover \Users\adamc\code\rf_classifier\models -v
 
 
 deactivate
