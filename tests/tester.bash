206682d12fb985827d6e039dc8234d70da4434fa
Adam Carpenter <adamc@jlab.org>
2019-08-20 08:26:38 -0400

    Updated tester to work with executable models

--- tests/tester.bash
+++ tests/tester.bash
@@ -1,22 +1,34 @@
 source ../venv/bin/activate
-export PYTHONPATH="../lib:../models:../venv/lib/site-packages"
+export PYTHONPATH="../lib:"
 
 echo "#"
 echo "# Running application tests"
 echo "#"
 python3 -m unittest discover . -v
 
-echo ""
-echo ""
-# Add the model directory to the environment's PYTHONPATH.  This allows for all of the needed imports.
-if [ "$(ls ../models | wc -w)" != '0' ] ; then
-    echo "#"
-    echo "# Running model tests"
-    echo "#"
-
-    python3 -m unittest discover ../models -v
-else
-    echo "# No models found to test"
-fi
-
+# Put the python environment back to stock
+export PYTHONPATH=""
 deactivate
+
+# Move on to test all of the models
+echo ""
+echo ""
+echo "#"
+echo "# Running model tests"
+echo "#"
+
+# List out each file/directory in the models dir. Exclude dot files like .gitignore
+for model in `ls ../models`
+do
+    echo "## $model ##"
+
+    # Load the models virtual environment
+    source ../models/$model/venv/bin/activate
+
+    # Run any unittests they have
+    python3 -m unittest discover "../models/$model/test" -v
+
+    # Unload the virtual environment
+    deactivate
+    echo ""
+done 
