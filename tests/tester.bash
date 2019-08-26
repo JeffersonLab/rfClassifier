4a36b13e7cc3565823dfc9181ca2320ba997b93b
Adam Carpenter <adamc@jlab.org>
2019-08-26 16:38:32 -0400

    Autodetects model venv dirs and activates them

--- tests/tester.bash
+++ tests/tester.bash
@@ -14,9 +14,18 @@ if [ "$(ls ../models | wc -w)" != '0' ] ; then
     echo "# Running model tests"
     echo "#"
 
-    python3 -m unittest discover ../models -v
+    for mod in $(ls ../models)
+    do
+        mod_dir="../models/$mod"
+        echo "# $mod"
+        if [ -d "$mod_dir/venv" ] ; then
+            source $mod_dir/venv/bin/activate
+        fi
+        python3 -m unittest discover ../models/$mod/test -v
+        if [ -d "$mod_dir/venv" ] ; then
+            deactivate
+        fi
+    done
 else
     echo "# No models found to test"
 fi
-
-deactivate
