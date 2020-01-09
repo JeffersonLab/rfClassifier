5044f13acfd4dfaa36c943407c24baeb4b05816b
Adam Carpenter <adamc@jlab.org>
2020-01-09 14:08:16 -0500

    Update release date and version

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -22,11 +22,11 @@ sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "lib")))
 # -- Project information -----------------------------------------------------
 
 project = 'rf_classifier'
-copyright = '2019, Adam Carpenter'
+copyright = '2020, Adam Carpenter'
 author = 'Adam Carpenter'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
