6b3d40b8384f6a043427556110365bf679c73093
Adam Carpenter <adamc@jlab.org>
2020-02-24 16:16:03 -0500

    Updates to work with current design of rf_classifier

--- bin/rf_classifier.ps1
+++ bin/rf_classifier.ps1
@@ -3,13 +3,19 @@ $scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
 $appDir = split-path -parent $scriptDir
 
 # Activate the apps python environment.
-& ${appDir}\venv\Scripts\Activate.ps1
-if (! $?) {
-    echo "Error activating python virutal environment.  Exiting."
-    exit 1
+if (Test-Path ${appDir}\venv) {
+    & ${appDir}\venv\Scripts\Activate.ps1
+    if (!$?)
+    {
+        echo "Error activating python virutal environment.  Exiting."
+        exit 1
+    }
 }
-
-$env:PYTHONPATH = "${appDir}\lib;${appDir}\models;${appDir}\venv\Lib\site-packages"
 
 # Run the app passing along all of the args
 python.exe ${appDIR}/lib/main.py $args
+
+# Deactivate the environment
+if (Test-Path ${appDir}\venv) {
+    deactivate
+}
