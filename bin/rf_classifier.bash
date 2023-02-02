2fa978dfe4e98e0571ca60897e33794ade2a1327
Adam Carpenter <adamc@jlab.org>
2023-02-02 11:09:43 -0500

    Update to use 'installed' app

--- bin/rf_classifier.bash
+++ bin/rf_classifier.bash
@@ -3,9 +3,12 @@
 # Get the directory containing this script
 DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" >/dev/null 2>&1 && pwd )"
 
-# Make sure our package search path is right
-export PATH="/usr/csite/pubtools/python/3.6.9/bin:$PATH"
-export PYTHONPATH="${DIR}/lib/:${DIR}/..:${PYTHONPATH}"
+# Activate the virtual environment
+source ${DIR}/../venv/bin/activate
 
 # Run the app passing along all of the args
-python3 ${DIR}/../lib/main.py "$@"
+python3 -m rf_classifier.main "$@"
+
+# Put the environment back the way it was.  Probably unnecesaary, but just in case the source does something really
+# unexpected
+deactivate
